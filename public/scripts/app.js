ed5ac67dec65348c5c8cdfbc707a66bd0901321e
Yan Yan Huang <yansquaredh@gmail.com>
2018-08-25 23:05:12 -0400

    adding a counter

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,18 +2,35 @@
 
 console.log('App.js is running');
 
+var app = {
+    title: 'Task App',
+    description: 'A CRUD App for tasks using React + Redux',
+    options: ['one', 'two']
+};
+
 var template = React.createElement(
     'div',
     null,
     React.createElement(
         'h1',
         null,
-        ' Yan Yan Huang '
+        ' ',
+        app.title,
+        ' '
+    ),
+    app.description && React.createElement(
+        'p',
+        null,
+        ' ',
+        app.description,
+        ' '
     ),
     React.createElement(
         'p',
         null,
-        ' This is some info '
+        ' ',
+        app.options.length > 0 ? 'Here are your options' : 'No options',
+        ' '
     ),
     React.createElement(
         'ol',
@@ -31,23 +48,42 @@ var template = React.createElement(
     )
 );
 
+var count = 0;
+var addOne = function addOne() {
+    console.log('add one');
+};
+
+var subOne = function subOne() {
+    console.log('subtract');
+};
+
+var resetCounter = function resetCounter() {
+    console.log('reset');
+};
+
 var template2 = React.createElement(
     'div',
     null,
     React.createElement(
         'h1',
         null,
-        ' Yan Yan '
+        ' Count: ',
+        count
     ),
     React.createElement(
-        'p',
-        null,
-        'Age: 25 '
+        'button',
+        { onClick: addOne },
+        ' +1 '
     ),
     React.createElement(
-        'p',
-        null,
-        'Location: NY '
+        'button',
+        { onClick: subOne },
+        ' -1 '
+    ),
+    React.createElement(
+        'button',
+        { onClick: resetCounter },
+        ' Reset '
     )
 );
 
