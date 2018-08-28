c1fc0568cd2c780689721e3c9b0e6d4230980517
Yan Yan Huang <yansquaredh@gmail.com>
2018-08-27 22:36:30 -0400

    added Options & AddOptions class

--- src/app.js
+++ src/app.js
@@ -2,14 +2,51 @@ console.log('New App.js');
 
 class Header extends React.Component {
     render() {
-        return <p> This is from the header </p> 
+        return (
+            <div>
+                <h1> To Do </h1>
+                <h2> Using React and Redux </h2>
+            </div>
+        )
+    }
+}
+
+class Action extends React.Component {
+    render () {
+        return (
+            <div>
+                <button> What should I do? </button>
+            </div>
+        )
+    }
+}
+
+class Options extends React.Component {
+    render () {
+        return (
+            <div>
+                See Options 
+            </div>
+        );
+    }
+}
+
+class AddOption extends React.Component {
+    render () {
+        return (
+            <div>
+                Adding Options 
+            </div>
+        );
     }
 }
 
 const jsx = (
     <div>
-        <h1> Title </h1>
         <Header />
+        <Action />
+        <Options />
+        <AddOption />
     </div>
 );
 
